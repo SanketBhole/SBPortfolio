8c7e6824c8929e5d0657ec03eda35f0a12ec2a4f
SanketBhole <sanketbhole123@gmail.com>
2024-02-18 11:59:04 +0530

    update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,24 +3,24 @@ import '../About/About.css'
 import Resume from '../../documents/Sanket_Bhole_Resume.pdf';
 import myabtimg from '../../images/myabout.png'
 import { motion } from "framer-motion";
-import { ImProfile} from 'react-icons/im';
-import {FaRegHandPointRight} from 'react-icons/fa'
+import { ImProfile } from 'react-icons/im';
+import { FaRegHandPointRight } from 'react-icons/fa'
 
 const pageVariants = {
   in: {
     opacity: 1,
     transition: "opacity 10s ease-in",
-    y:0
+    y: 0
   },
   out: {
     opacity: 0,
     transition: "opacity 10s ease-in",
-    y:"100%"
+    y: "100%"
   },
 }
 export default function About() {
   return (
-    <motion.div className='text-light'id='aboutsection' initial="out" animate="in" exit="out" variants={pageVariants}>
+    <motion.div className='text-light' id='aboutsection' initial="out" animate="in" exit="out" variants={pageVariants}>
       <div className="container p-1">
         <div className="row p-5 justify-content-sm-center mainrow">
           <div className="col-sm-5 p-0 text-center">
@@ -32,15 +32,15 @@ export default function About() {
             <h1 className="about-title">Summary</h1>
             <div className="about-item pb-0">
               <h3>Sanket Bhole</h3>
-              <p>I am a Full Stack Developer who is passionate about learning new technologies and believes in the importance of continuous self-improvement and adaptability in the ever-evolving field of computer science. 
+              <p>I am a Full Stack Developer who is passionate about learning new technologies and believes in the importance of continuous self-improvement and adaptability in the ever-evolving field of computer science.
                 My dedication to excellence, ability to work under tight deadlines, and commitment to staying up-to-date with the latest industry trends make me a valuable asset to any team.</p>
-              <p/>
+              <p />
               <ul>
                 <li>Hosuing Board Colony, Pithampur, MadhyaPradesh</li>
                 <li>+91-78987 92951</li>
                 <li>er.sanketbhole@gmail.com</li>
               </ul>
-              <p><div className="p-2"><a className='profile' href={Resume} rel="noreferrer" target='_blank'><ImProfile/> View Full Resume <FaRegHandPointRight/></a></div></p>
+              <p><div className="p-2"><a className='profile' href={Resume} rel="noreferrer" target='_blank'><ImProfile /> View Full Resume <FaRegHandPointRight /></a></div></p>
             </div>
 
             <h1 className="about-title">Education</h1>
@@ -58,55 +58,70 @@ export default function About() {
             </div>
             <div className="about-item">
               <h3>Secondary</h3>
-              <h4>2018 - 2019</h4>
+              <h4>2016 - 2017</h4>
               <p><em>Little Angels Higher Secondary School, MHOW, Madhya Pradesh</em></p>
-              <p>Completed with CGPA of 7.6.</p>
+              <p>Completed with CGPA of 7.2.</p>
             </div>
             <h1 className="about-title">Skills</h1>
             <div className="row">
               <div className="col-sm-6 progressbars">
+
                 <div className="progress">
-                  <span className="skill">HTML <i className="val">100%</i></span>
+                  <span className="skill">Java <i className="val">80%</i></span>
                   <div className="progress-bar-wrap">
-                    <div className="progress-bar" role="progressbar" style={{width:'100%'}} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div className="progress-bar" role="progressbar" style={{ width: '80%' }} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                </div>
+
+                <div className="progress">
+                  <span className="skill">HTML <i className="val">90%</i></span>
+                  <div className="progress-bar-wrap">
+                    <div className="progress-bar" role="progressbar" style={{ width: '90%' }} aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
                 </div>
 
                 <div className="progress">
                   <span className="skill">CSS <i className="val">80%</i></span>
                   <div className="progress-bar-wrap">
-                    <div className="progress-bar" role="progressbar" style={{width:'80%'}} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div className="progress-bar" role="progressbar" style={{ width: '80%' }} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
                 </div>
+
+              </div>
+
+              <div className="col-sm-6 progressbars">
 
                 <div className="progress">
                   <span className="skill">Bootstrap <i className="val">90%</i></span>
                   <div className="progress-bar-wrap">
-                    <div className="progress-bar" role="progressbar" style={{width:'90%'}} aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div className="progress-bar" role="progressbar" style={{ width: '90%' }} aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
                 </div>
+
+                <div className="progress">
+                  <span className="skill">Java Script <i className="val">80%</i></span>
+                  <div className="progress-bar-wrap">
+                    <div className="progress-bar" role="progressbar" style={{ width: '80%' }} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                </div>
+
+                <div className="progress">
+                  <span className="skill">React Js<i className="val">80%</i></span>
+                  <div className="progress-bar-wrap">
+                    <div className="progress-bar" role="progressbar" style={{ width: '80%' }} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                </div>
+
               </div>
               <div className="col-sm-6 progressbars">
+
                 <div className="progress">
-                  <span className="skill">React Js <i className="val">60%</i></span>
+                  <span className="skill">Oracle<i className="val">70%</i></span>
                   <div className="progress-bar-wrap">
-                    <div className="progress-bar" role="progressbar" style={{width:'60%'}} aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div className="progress-bar" role="progressbar" style={{ width: '70%' }} aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
                 </div>
 
-                <div className="progress">
-                  <span className="skill">MySql <i className="val">70%</i></span>
-                  <div className="progress-bar-wrap">
-                    <div className="progress-bar" role="progressbar"style={{width:'70%'}} aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
-                  </div>
-                </div>
-
-                <div className="progress">
-                  <span className="skill">Core Java <i className="val">75%</i></span>
-                  <div className="progress-bar-wrap">
-                    <div className="progress-bar" role="progressbar" style={{width:'75%'}} aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
